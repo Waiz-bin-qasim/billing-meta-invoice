57eab8da566486151cdffde5156eb6b4c6bc10fb
Waiz-Bin_qasim <103220607+Waiz-bin-qasim@users.noreply.github.com>
2023-08-25 17:50:50 +0500

    upload page 50% completed

--- Views/Static/Js/Upload.js
+++ Views/Static/Js/Upload.js
@@ -1,58 +1,25 @@
-//selecting all required elements
-const dropArea = document.querySelector(".drag-area"),
-  dragText = dropArea.querySelector("header"),
-  button = dropArea.querySelector("button"),
-  input = dropArea.querySelector("input");
-let file; //this is a global variable and we'll use it inside multiple functions
+const dropArea = document.querySelector("#drop-area");
+const inputFile = document.querySelector("#input-file");
+const imageView = document.querySelector("#img-view");
+const newImage = document.createElement("img");
 
-button.onclick = () => {
-  input.click(); //if user click on the button then the input also clicked
-};
+// const uploadFile = () => {
+//   let imageLink = URL.createObjectURL(inputFile.files[0]);
+//   console.log(imageLink);
 
-input.addEventListener("change", function () {
-  //getting user select file and [0] this means if user select multiple files then we'll select only the first one
-  file = this.files[0];
-  dropArea.classList.add("active");
-  showFile(); //calling function
-});
-//If user Drag File Over DropArea
-dropArea.addEventListener("dragover", (event) => {
-  event.preventDefault(); //preventing from default behaviour
-  dropArea.classList.add("active");
-  dragText.textContent = "Release to Upload File";
-});
+// };
 
-//If user leave dragged File from DropArea
-dropArea.addEventListener("dragleave", () => {
-  dropArea.classList.remove("active");
-  dragText.textContent = "Drag & Drop to Upload File";
-});
+// inputFile.addEventListener("change", uploadFile);
 
-//If user drop File on DropArea
-dropArea.addEventListener("drop", (event) => {
-  event.preventDefault(); //preventing from default behaviour
-  //getting user select file and [0] this means if user select multiple files then we'll select only the first one
-  file = event.dataTransfer.files[0];
-  showFile(); //calling function
-});
-
-function showFile() {
-  let fileType = file.type; //getting selected file type
-  let validExtensions = ["image/pdf", "image/jpg", "image/png"]; //adding some valid image extensions in array
-  if (validExtensions.includes(fileType)) {
-    //if user selected file is an image file
-    let fileReader = new FileReader(); //creating new FileReader object
-    fileReader.onload = () => {
-      console.log(file);
-      let fileURL = fileReader.result; //passing user file source in fileURL variable
-      let imgTag = `<img src="${fileURL}" alt="image">`; //creating an img tag and passing user selected file source inside src
-      attribute;
-      dropArea.innerHTML = imgTag; //adding that created img tag inside dropArea container
-    };
-    fileReader.readAsDataURL(file);
+const loadFile = function (event) {
+  var image = document.getElementById("show-uploaded-image");
+  if (event.target.files[0].type === "application/pdf") {
+    image.src = "../Static/Img/pdf-icon.webp";
+    document.querySelector(".para-text").innerHTML =
+      "File has been Uploaded <br/> Click Submit";
+    document.querySelector(".span-text").innerText = event.target.files[0].name;
   } else {
-    alert("This is not an Image File!");
-    dropArea.classList.remove("active");
-    dragText.textContent = "Drag & Drop to Upload File";
+    alert("Incorrect File Type");
+    event.target.files[0] = null;
   }
-}
+};
