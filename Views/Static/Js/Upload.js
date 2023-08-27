9fd9b1f6424e8598d6e2efad63d577ad96a776ea
Waiz-Bin_qasim <103220607+Waiz-bin-qasim@users.noreply.github.com>
2023-08-27 17:28:14 +0500

    event listerner was added to send the file to api

--- Views/Static/Js/Upload.js
+++ Views/Static/Js/Upload.js
@@ -2,24 +2,53 @@ const dropArea = document.querySelector("#drop-area");
 const inputFile = document.querySelector("#input-file");
 const imageView = document.querySelector("#img-view");
 const newImage = document.createElement("img");
-
-// const uploadFile = () => {
-//   let imageLink = URL.createObjectURL(inputFile.files[0]);
-//   console.log(imageLink);
-
-// };
-
-// inputFile.addEventListener("change", uploadFile);
+const formSubmit = document.querySelector("form");
+const oldParserbtn = document.querySelector("#oldParser");
+const newParserbtn = document.querySelector("#newParser");
 
 const loadFile = function (event) {
   var image = document.getElementById("show-uploaded-image");
-  if (event.target.files[0].type === "application/pdf") {
+  if (
+    event.target.files[0] &&
+    event.target.files[0].type === "application/pdf"
+  ) {
     image.src = "../Static/Img/pdf-icon.webp";
     document.querySelector(".para-text").innerHTML =
-      "File has been Uploaded <br/> Click Submit";
+      "File has been Uploaded <br/> Select The Parser";
     document.querySelector(".span-text").innerText = event.target.files[0].name;
   } else {
-    alert("Incorrect File Type");
+    image.src = "../Static/Img/508-icon.png";
     event.target.files[0] = null;
   }
 };
+
+function submitForm(form, parserType) {
+  console.log(document.activeElement.value);
+  console.log("waiz");
+  if (document.activeElement.value == parserType) {
+    console.log("Have one.");
+    return true;
+  }
+  return false;
+}
+
+formSubmit.addEventListener("submit", function (event) {
+  const form = event.currentTarget;
+  if (form.file.files[0]) {
+    const formData = new FormData(form);
+    const url = "";
+    if (submitForm(form, "Old Parser")) {
+      formData.parserChoice = 0;
+    } else if (submitForm(form, "New Parser")) {
+      formData.parserChoice = 1;
+    }
+    const fetchOptions = {
+      method: "POST",
+      body: formData,
+    };
+    fetch(url, fetchOptions);
+  } else {
+    alert("No File Uploaded");
+  }
+  event.preventDefault();
+});
