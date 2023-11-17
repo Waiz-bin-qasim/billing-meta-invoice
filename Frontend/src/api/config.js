0d19e7118396b2a7a3b50ba8c40b5317e2c4d972
Waiz-bin-qasim <waizbinqasim0@gmail.com>
2023-11-17 09:41:08 +0500

    changing the backend ip address

--- Frontend/src/api/config.js
+++ Frontend/src/api/config.js
@@ -1,4 +1,4 @@
 export const config = {
-    url : "http://127.0.0.1:8090/"
-}
-export const getToken = localStorage.getItem('token')
+  url: "http://52.220.227.74:8090/",
+};
+export const getToken = localStorage.getItem("token");
